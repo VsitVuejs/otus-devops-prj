a166ba47a64639d0a2d409bf27587b9ef50ba487
vsit <12345@gmail.com>
2023-09-02 13:45:27 +0500

    change role iam

--- terraform/cluster/modules/bucket/main.tf
+++ terraform/cluster/modules/bucket/main.tf
@@ -14,7 +14,7 @@ resource "yandex_iam_service_account" "sa" {
 
 resource "yandex_resourcemanager_folder_iam_member" "sa-editor" {
   folder_id = var.folder_id
-  role      = "storage.admin"
+  role      = "editor"
   member    = "serviceAccount:${yandex_iam_service_account.sa.id}"
 }
 
